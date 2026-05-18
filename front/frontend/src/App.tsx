bf9b833138f192f223eb20b60a4687f761b22804
ilkdy1234567-ui <ilkdy1234567@gmail.com>
2026-05-18 16:05:48 +0900

    조직도 작업중

--- front/frontend/src/App.tsx
+++ front/frontend/src/App.tsx
@@ -8,13 +8,14 @@ function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-gray-50">
+
         {/* 헤더는 모든 페이지에서 공통으로 보입니다 */}
         <Header />
         
         {/* 실제 콘텐츠가 바뀌는 영역 */}
         <main className="max-w-7xl mx-auto p-6">
           <Routes>
-            <Route path="/org" element={<OrgPage />} />
+            <Route path="/" element={<OrgPage />} />
             <Route path="/org" element={<OrgPage />} />
             <Route path="/board" element={<PostPage />} />
             <Route path="/admin" element={<AdminPage />} />
